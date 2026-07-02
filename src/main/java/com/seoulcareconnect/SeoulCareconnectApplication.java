d1046c8afea67e465b7908e1bb75dc0126225544
jiho031 <wlgmlgksfk@gmail.com>
2026-07-02 09:39:28 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/seoulcareconnect/SeoulCareconnectApplication.java
@@ -0,0 +1,13 @@
+package com.seoulcareconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulCareconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeoulCareconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seoulcareconnect/SeoulCareconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoulcareconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulCareconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
